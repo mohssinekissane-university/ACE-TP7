fca539c4246c35a20698350995a0904fe9bc5786
mohssinekissane <kissanemohssine2001@gmail.com>
2025-10-29 23:00:24 +0100

    first commit

---
+++ src/main/java/com/mohssinekissane/acetp7/AceTp7Application.java
@@ -0,0 +1,13 @@
+package com.mohssinekissane.acetp7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceTp7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceTp7Application.class, args);
+    }
+
+}
